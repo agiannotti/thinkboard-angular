0b560cdff03e6a0d23c241ec0d6c6e2caf39a81d
Anthony Giannotti <aj.gianno@gmail.com>
2021-04-28 22:24:26 -0500

    !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'thinkboard-angular';
+  todo: Task[] = [
+    {
+      title: 'Finish Projects',
+      description: 'Many commits',
+    },
+    {
+      title: 'Feed Cats',
+      description: 'Go to store and buy cat food',
+    },
+    {
+      title: 'Learn piano',
+      description: 'Attend Piano lessons',
+    },
+    {
+      title: 'Find a job',
+      description: 'invert binary trees',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { TaskComponent } from './src/task/task.component';
+import { MatCardModule } from '@angular/material/card';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [AppComponent, TaskComponent],
@@ -16,6 +17,7 @@ import { TaskComponent } from './src/task/task.component';
     BrowserAnimationsModule,
     MatIconModule,
     MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
