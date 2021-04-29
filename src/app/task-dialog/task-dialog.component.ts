03bcf2896b566cfdff935e60f67fa07e35f1c396
Anthony Giannotti <aj.gianno@gmail.com>
2021-04-29 02:31:29 -0500

    bugs via todo typo

--- src/app/task-dialog/task-dialog.component.ts
+++ src/app/task-dialog/task-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Task } from '../task/task';
 
 @Component({
@@ -7,16 +7,16 @@ import { Task } from '../task/task';
   templateUrl: './task-dialog.component.html',
   styleUrls: ['./task-dialog.component.css'],
 })
-export class TaskDialogComponent implements OnInit {
+export class TaskDialogComponent {
   private backupTask: Partial<Task> = { ...this.data.task };
   constructor(
-    publicdialogRef: MatDialogRef<TaskDialogComponent>,
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: TaskDialogData
   ) {}
 
   cancel(): void {
-    this.data.task.title = this.backupTask.title;
-    this.data.task.description = this.backupTask.description;
+    // this.data.task.title = this.backupTask.title;
+    // this.data.task.description = this.backupTask.description;
     this.dialogRef.close(this.data);
   }
 
